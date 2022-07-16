97d9c7c7bfe89e7b5b1670310a35528160324fd2
Daniel Chiang <chunyi@uab.edu>
2022-07-15 22:31:36 -0500

    file position managing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import redux,{Provider} from "react-redux";
-import Movie from "./MovieComponent";
+import Movie from "./components/movie1/MovieComponent";
 
 function App() {
     return (
-      <div>
-        <Movie />
-      </div>
+      <>
+        <h1> Hello World! </h1>
+      </>
     );
   }
-  
+
   export default App;
