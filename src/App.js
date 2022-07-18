7e50025e6d5d65527cf87d958d7e24d4280b3cb9
Alan Chiu <ms0372631@gmail.com>
2022-07-17 21:35:26 -0700

    useContext setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navbar } from "./components/navbar";
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useState, useC } from 'react';
 import redux, { Provider } from "react-redux";
 import { Selector } from "./selector";
 import "./App.sass";
@@ -8,13 +8,29 @@ import Movie from "./components/movie1/MovieComponent";
 import Sort from "./components/sort/Sort";
 
 function App() {
+    const [likeList, setLikeList] = useState([]);
+    const [blockList, setBlockList] = useState([]);
+    const [popMovies, setPopMovies] = useState([]);
+    const [movieList, setmovieList] = useState([]);
+
+    const allMovieLists = {
+      likeList,
+      setLikeList,
+      blockList,
+      setBlockList,
+      movieList,
+      setmovieList
+    };
+
+    const MovieContext = React.createContext(allMovieLists);
+
     return (
-      <>
+      <MovieContext.Provider value={MovieContext}>
         <Navbar />
         <Selector />
         <Sort />
-        <Movie />
-      </>
+        <Movie/>
+      </MovieContext.Provider>
     );
   }
 
