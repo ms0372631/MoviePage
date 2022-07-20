5db4530d13bac39ca631f9cbf7c006a1929b4b38
Daniel Chiang <chunyi@uab.edu>
2022-07-20 16:35:30 -0500

    update API and props in homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {MovieComponent} from "./components/movie1/MovieComponent";
+import { getURL } from "./components/API";
 import "./HomePage.css";
 
 export const HomePage = () => {
@@ -11,7 +12,7 @@ export const HomePage = () => {
 
 
     useEffect(() => {
-        const url='https://api.themoviedb.org/3/movie/now_playing?api_key=cb12866696d0696b62106d1f1101392a';
+        const url=getURL("playing");
         fetch(url).then(response=>response.json())
         .then(response=>setMovies(response.results))
     }, []);
@@ -25,12 +26,7 @@ export const HomePage = () => {
             <MovieComponent
             key={movie.id}
             id={movie.id}
-            name={movie.title}
-            poster_path={movie.poster_path}
-            date={movie.release_date}
-            vote_average={movie.vote_average}
-            vote_count={movie.vote_count}
-            overview={movie.overview}/>
+            movie={movie}/>
             ))}
         </div>
         </div>
