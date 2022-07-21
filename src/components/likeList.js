4cb0f405c986d712e5f3b5e99743355e1ab60cd2
kongjunyihao <xutianxiang47@gmail.com>
2022-07-21 01:14:36 -0700

    finish like list and block list

--- src/components/likeList.js
+++ src/components/likeList.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { MovieComponent2 } from "./movie1/MovieComponent2";
+import { IMG_API } from "../API";
+import { MovieContext } from "../App";
+import { iteratorSymbol } from "immer/dist/internal";
 
 export const LikeList = () => {
   // movieContext.savedList[].like => true -> in like list
   // movieContext.savedList[].like => false -> in block list
   // movieContext.savedList[].movie => movie_data
+  const movieContext = useContext(MovieContext);
 
   return (
     <>
-      <MovieComponent2 />
+      {movieContext.savedList
+       .filter(item=>item.like)
+       .map(item =><MovieComponent2
+       key={item.movie.id}
+       item={item} />)}
     </>
-  )
+  );
 }
