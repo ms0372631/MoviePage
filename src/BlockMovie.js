697c57b68c56b06d13d05ef12a379c62d9a45f7d
kongjunyihao <xutianxiang47@gmail.com>
2022-07-20 20:59:58 -0700

    Create BlockMovie.js

---
+++ src/BlockMovie.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import {useState, useEffect} from 'react';
+import "./LikeMovie.css"
+
+
+const BlockMovie = () => {
+    const [data, setData] = useState(null);
+    const [images, setImage] = useState([]);
+
+    useEffect(() => {
+        fetch(`url`).then((response) => response.json())
+        .then(data => setData(data))
+    }, [data]);
+
+    const handleDelete = (id) => {
+        setImage((prevState) => prevState.filter((item) => item.id !== id));
+    };
+
+    const handleBlock = (id) => {
+
+    }
+    
+    return (
+        <div classNamme="Like Movie">
+        {images.map((image) => {
+            return(
+                <div>
+                <img src='sea.jpg' alt='sea'></img>
+                <button onClick={() => handleDelete(image.id)} className="delete">Delete</button>
+                <button onClick={() => handleBlock(image.id)} className="block">Block</button>
+                </div>
+            );
+        })}
+        </div>
+    );
+};
+export default BlockMovie;
