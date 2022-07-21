8e3fef16a08d0b1e21181bfc8b65fc1f68878c87
kongjunyihao <xutianxiang47@gmail.com>
2022-07-21 01:18:26 -0700

    Update blockList.js

--- src/components/blockList.js
+++ src/components/blockList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
+import { MovieComponent2 } from "./movie1/MovieComponent2";
+import { IMG_API } from "../API";
+import { MovieContext } from "../App";
 
-export const Blockist = () => {
-
+export const BlockList = () => {
+  // movieContext.savedList[].like => true -> in like list
+  // movieContext.savedList[].like => false -> in block list
+  // movieContext.savedList[].movie => movie_data
+  const movieContext = useContext(MovieContext);
 
   return (
-    <>List of BlockLists</>
-  )
+    <>
+      {movieContext.savedList
+       .filter(item=>!item.like)
+       .map(item =><MovieComponent2
+       key={item.movie.id}
+       item={item} />)}
+    </>
+  );
 }
